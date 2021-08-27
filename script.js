dfb7af1640ae35de6fcca5294ea7fdececa16348
yeerajan <70102636+yeerajan@users.noreply.github.com>
2021-08-27 09:04:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 898; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+    const label = document.createElement('p');
+    label.innerText = `#${i}`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
